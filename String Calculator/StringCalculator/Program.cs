dc44e7aec6baaae9edae50001ee606132b778846
Derek Thiessen <thiessd@gmail.com>
2013-02-28 18:37:31 -0500

    Add string calculator project

---
+++ String Calculator/StringCalculator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculator
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("String Calculator");
+            Console.ReadLine();
+        }
+    }
+}
