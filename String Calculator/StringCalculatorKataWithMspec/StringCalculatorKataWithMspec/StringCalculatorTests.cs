757ecd721e70be6e4ce78eda6c911c61398a597a
Derek Thiessen <thiessd@gmail.com>
2013-03-01 07:39:16 -0500

    added solution

---
+++ String Calculator/StringCalculatorKataWithMspec/StringCalculatorKataWithMspec/StringCalculatorTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Machine.Specifications;
+
+namespace StringCalculatorKataWithMspec
+{
+    class when_empty_string
+    {
+        static StringCalculator stringCalculator;
+        static int returnValue;
+
+        Establish context = () => stringCalculator = new StringCalculator();
+
+        Because of =()=> returnValue = stringCalculator.Add("");
+
+        It should_return_zero = () => returnValue.ShouldEqual(0);
+    }
+}
