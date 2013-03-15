f589a677ef2246f50793efaf2ca0636f57bdc333
Derek Thiessen <thiessd@gmail.com>
2013-03-15 08:52:55 -0400

    Create FizzBuzz solution with machane specifications

---
+++ FizzBuzz/FizzBuzz/FizzBuzz/FizzBuzzTests.cs
@@ -0,0 +1,15 @@
+﻿using Machine.Specifications;
+
+namespace FizzBuzz
+{
+	class FizzBuzzTests
+	{
+		private static FizzBuzz fizzBuzz;
+
+		private Establish context = () => fizzBuzz = new FizzBuzz();
+
+		private Because of = () => { };
+
+		private It should_return_zero = () => { };
+	}
+}
